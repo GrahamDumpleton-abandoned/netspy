ce0e40bcd4c29493aeb176e8ecf9a800edf0c0a1
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-02-11 16:05:32 +1100

    Wrong module name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-  name = "newrelic",
+  name = "netspy",
   version = "1.0.0",
   description = "GUI debugger for OSE message system.",
   author = "Graham Dumpleton",
