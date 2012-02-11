7342c8674ae5aebc05e4eac7c74edccda3412c53
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-02-11 15:53:24 +1100

    Initialise git repo.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = "newrelic",
+  version = "1.0.0",
+  description = "GUI debugger for OSE message system.",
+  author = "Graham Dumpleton",
+  author_email = "Graham.Dumpleton@gmail.com",
+  packages = ['netspy'],
+  scripts = [ 'scripts/netspy', 'scripts/netspy-registry',
+              'scripts/netspy-reports' ],
+)
